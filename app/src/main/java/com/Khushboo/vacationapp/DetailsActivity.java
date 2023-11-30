c42a3efc0e3e13686d66b9e51ca1ca8bd5303576
Khushboo Agarwal <threesisters3331@gmail.com>
2023-12-01 00:04:43 +0530

    Final commit

---
+++ app/src/main/java/com/Khushboo/vacationapp/DetailsActivity.java
@@ -0,0 +1,13 @@
+package com.Khushboo.vacationapp;
+import androidx.appcompat.app.AppCompatActivity;
+import com.khushboo.vacationapp.R;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/Khushboo/vacationapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.Khushboo.vacationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.Khushboo.vacationapp.adapter.TopPlacesAdapter;
+import com.Khushboo.vacationapp.model.TopPlacesData;
+import com.khushboo.vacationapp.R;
+import com.Khushboo.vacationapp.adapter.RecentsAdapter;
+import com.Khushboo.vacationapp.model.RecentsData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recentRecycler, topPlacesRecycler;
+    RecentsAdapter recentsAdapter;
+    TopPlacesAdapter topPlacesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        List<RecentsData> recentsDataList = new ArrayList<>();
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+        recentsDataList.add(new RecentsData("AM Lake","India","From $200",R.drawable.recentimage1));
+        recentsDataList.add(new RecentsData("Nilgiri Hills","India","From $300",R.drawable.recentimage2));
+
+        setRecentRecycler(recentsDataList);
+
+        List<TopPlacesData> topPlacesDataList = new ArrayList<>();
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+        topPlacesDataList.add(new TopPlacesData("Kasimir Hill","India","$200 - $500",R.drawable.topplaces));
+
+        setTopPlacesRecycler(topPlacesDataList);
+    }
+
+    private  void setRecentRecycler(List<RecentsData> recentsDataList){
+
+        recentRecycler = findViewById(R.id.recent_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        recentRecycler.setLayoutManager(layoutManager);
+        recentsAdapter = new RecentsAdapter(this, recentsDataList);
+        recentRecycler.setAdapter(recentsAdapter);
+
+    }
+
+    private  void setTopPlacesRecycler(List<TopPlacesData> topPlacesDataList){
+
+        topPlacesRecycler = findViewById(R.id.top_places_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        topPlacesRecycler.setLayoutManager(layoutManager);
+        topPlacesAdapter = new TopPlacesAdapter(this, topPlacesDataList);
+        topPlacesRecycler.setAdapter(topPlacesAdapter);
+
+    }
+
+
+}
